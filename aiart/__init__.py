031c00036c47e75b9bb6f80f9309ac4eaf0de261
kamangir <arash@kamangir.net>
2023-03-20 16:28:10 -0700

    += html - kamangir/bolt#748

--- aiart/__init__.py
+++ aiart/__init__.py
@@ -1,5 +1,5 @@
 NAME = "aiart"
 
-VERSION = "1.24.1"
+VERSION = "1.25.1"
 
 from .functions import *
---
+++ aiart/html.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+from abcli import logging
+from abcli.logging import crash_report
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def ingest_url(url):
+    """ingest poetry from url.
+
+    Args:
+        url (str): example: https://allpoetry.com/16-bit-Intel-8088-chip
+
+    Returns:
+        bool: success
+        List[str]: content
+    """
+    try:
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        title = soup.find("h1", {"class": "title"}).text.strip()
+
+        poem_body = (
+            soup.find("div", {"class": "poem_body"})
+            .find_all("div")[1]
+            .text.strip()
+            .split("\n")
+        )
+
+        logger.info(
+            "{}: {} line(s): {}".format(title, len(poem_body), ", ".join(poem_body))
+        )
+
+        return True, [title] + poem_body
+    except:
+        crash_report()
+        return False, []
