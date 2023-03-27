459256fe8d46dc030b40f1f895bff2c51bb4a8c8
kamangir <arash@kamangir.net>
2023-03-26 17:07:46 -0700

    += help - kamangir/bolt#748

--- .abcli/aiart.sh
+++ .abcli/aiart.sh
@@ -4,6 +4,13 @@ function aiart() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
+        abcli_show_usage "abcli_quote <message>" \
+            "urllib.parse.quote(<message>)."
+        abcli_show_usage "abcli_unquote <message>" \
+            "urllib.parse.unquote(<message>)."
+        aiart_generate help app=aiart
+        aiart_transform "$@"
+
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
             python3 -m aiart --help
         fi
