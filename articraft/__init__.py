531c79e19df944f3f6b123b632aed24e0489f1c1
kamangir <arash@kamangir.net>
2024-06-15 22:30:51 -0700

    += marquee - kamangir/bolt#746

--- articraft/__init__.py
+++ articraft/__init__.py
@@ -4,4 +4,6 @@ ICON = "🎨"
 
 DESCRIPTION = f"{ICON} tools for ai artists."
 
-VERSION = "2.53.1"
+VERSION = "2.54.1"
+
+MARQUEE = "https://github.com/kamangir/openai-commands/raw/main/assets/DALL-E.png?raw=1"
